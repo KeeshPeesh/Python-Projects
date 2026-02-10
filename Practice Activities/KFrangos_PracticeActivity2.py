acc4627c7c51a99a4506bed0d26f98d95239e59b
KeeshPeesh <38126@ocvts.org>
2026-02-10 12:32:33 -0500

    Butter

---
+++ Practice Activities/KFrangos_PracticeActivity2.py
@@ -0,0 +1,21 @@
+# Practice Activity 1
+name = input("Name: ")
+age = int(input("Age: "))
+print("Hello " + name + ", you are " + int(age) + " years old.")
+
+# Practice Activity 2
+age = int(input("Age: "))
+if age < 18:
+    print("Minor")
+elif age >= 18:
+    print("Adult")
+
+# Practice Activity 3
+option = input("Choose an option (1, 2, or 3): ")
+match option:
+    case "1":
+        print("You chose option 1")
+    case "2":
+        print("You chose option 2")
+    case "3":
+        print("You chose option 3")
