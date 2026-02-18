471455bf4d92122d506759ddb151ba1e8909b435
KeeshPeesh <38126@ocvts.org>
2026-02-18 11:54:18 -0500

    2/18/26 - PracticeActivity3

---
+++ Practice Activities/KFrangos_PracticeActivity3.py
@@ -0,0 +1,10 @@
+#Practice Activity 1
+while True:
+    userInput = input("Type stop to end the program: ")
+    if userInput == "stop":
+        break
+    
+#Practice Activity 2
+for i in range(1, 11):
+    print(i)
+    
