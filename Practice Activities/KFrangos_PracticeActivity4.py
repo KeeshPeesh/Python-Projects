751c9766039af84965a7589e4962bebf55fb95a2
KeeshPeesh <38126@ocvts.org>
2026-02-19 11:09:00 -0500

    2/19/26 - Practice Activity 4

---
+++ Practice Activities/KFrangos_PracticeActivity4.py
@@ -0,0 +1,15 @@
+#Practice Activity 1
+def square(num):
+    return num ** 2
+
+print(square(5))
+
+#Practice Activity 2
+name = input("Enter your name: ")
+for letter in name:
+    print(letter)
+    
+#Practice Activity 3
+import random
+for i in range(5):
+    print(random.randint(1, 100))
