68cdbd3a24e864469d680b604525a7b5e8f13a9c
KeeshPeesh <38126@ocvts.org>
2026-03-04 10:51:03 -0500

    fixing

---
+++ Practice Activities/KFrangos_PracticeActivity5.py
@@ -0,0 +1,7 @@
+# Create a list of
+# 5 numbers and
+# print each one.
+
+num = [5, 4, 3, 2, 1]
+for i in num:
+    print(i)
---
+++ Practice Activities/KFrangos_PracticeActivity6.py
@@ -0,0 +1,12 @@
+# Create a list of
+# numbers and
+# calculate the
+# average.
+
+num = [ 10, 20, 30, 40, 50 ]
+total = sum((num))
+average = total / len(num)
+
+print("The average is:", average)
+
+#What is Data Science? - field that uses methods, processes, algorithms and systems to get information from structured and unstructured data
